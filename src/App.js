109691610ceff51338aea2534a208a74996e3839
hieuntcasso <94815095+hieuntcasso@users.noreply.github.com>
2022-05-30 20:09:28 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
+import './App.css';
+import React from 'react';
+//pages
+import Home from './pages/Home'
+// const component
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+const configComponent = (component) => {
+    return (
+        <div>
+            <Header />
+            <div className="mt-12 mb-[60px]">
+                {component}
+            </div>
+
+            <Footer />
+        </div>
+    )
+}
+let path = [
+ 
+    {
+        path: '/dashboard',
+        component: configComponent(<Home />)
+    },
+  
+]
+
+
+function App() {
+    return (
+        <Routes>
+            {path.map((item, index) => {
+                return (
+                    <Route path={item.path} element={item.component} key={index} />
+                )
+            })}
+        </Routes>
+    );
+}
+
+export default App;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react'
+
+const Header = () => {
+  
+
+    return (
+        <div className="fixed top-0 left-0 right-0 z-10  text-[#003663] font-bold grid grid-cols-4 px-8 shadow-md h-12 bg-white	">
+          
+        </div>
+    )
+}
+
+export default Header;
